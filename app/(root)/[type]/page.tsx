417499855123729d4fabc265e7a6fa3f70620b79
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-12-15 03:40:17 +0530

    basic skeleton of homepage and individual blog page is ready

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { redirect } from 'next/navigation';
-import Dropdown from './blogdropdown';
+import React from 'react'
+import { redirect } from 'next/navigation'
+import Dropdown from './blogdropdown'
 
-
-  
-
+const Page = async ({ params }: { params: { type: string } }) => {
+  const resolvedParams = await params // Await the params object
+  const type = resolvedParams.type || '' // Access `type` safely
+  if (!type) {
+    redirect('/')
+  }
   // Render Dropdown with the type parameter
   return (
     <div className="p-6">
       <Dropdown params={{ type }} />
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
