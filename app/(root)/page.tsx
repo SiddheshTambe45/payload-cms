913898010941b6e0a6c9218105b1d56c7d89bd40
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-12-15 14:06:59 +0530

    upcomings image16/9

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -71,33 +71,39 @@
 // export default Page
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
-  const leftImageTitle =
-    'Nintendo Museum – A Journey Through Time Nintendo Museum – A Journey Through Time Nintendo Museum – A Journey Through Time '
+  const leftImageTitle = 'Nintendo Museum – A Journey Through Time Nintendo Muse'
   const rightImageTitle = 'Exploring the Legacy of Nintendo'
 
   return (
     <div className="flex flex-col justify-start h-screen max-w-7xl mx-auto px-2 md:px-1 lg:px-0">
-      <section className="featured-blogs w-full">
+      <section className="featured-blogs w-full p-3">
         <div className="featured-blogs-upper grid grid-cols-1 lg:grid-cols-3 gap-4 h-auto">
           {/* Left Div */}
-          <div className="col-span-2 flex flex-col items-start">
-            <div className="relative w-full aspect-[16/9]">
-              <Image
-                src="/assets/images/image15.png"
-                alt="left image"
-                fill
-                className="object-cover"
-              />
+          <Link className="p-0 m-0 col-span-2 flex flex-col items-start" href="/games/123">
+            {/* <div className="col-span-2 flex flex-col items-start"> */}
+            {/* Fixed Height with Aspect Ratio */}
+            <div className="relative h-[400px] w-full">
+              <div className="relative w-full h-full aspect-[16/9]">
+                <Image
+                  src="/assets/images/image15.png"
+                  alt="left image"
+                  fill
+                  className="object-cover"
+                />
+              </div>
             </div>
-            <p className="mt-2 text-left text-lg font-semibold">{leftImageTitle}</p>
-          </div>
+            {/* Text */}
+            <p className="mt-2 text-left text-lg font-semibold leading-tight">{leftImageTitle}</p>
+            {/* </div> */}
+          </Link>
 
           {/* Right Div */}
           <div className="col-span-1 flex flex-col items-start">
-            <div className="relative w-full h-full">
+            <div className="relative w-full h-[400px]">
               <Image
                 src="/assets/images/image16.png"
                 alt="right image"
@@ -209,24 +215,43 @@ const Page = () => {
         <section className="lg:col-span-1 px-3">
           <div className="border-gray-400 border w-full h-[230px] my-4"></div>
 
-          {[0, 1, 2, 3, 4, 5].map((e) => (
-            <div
-              key={e}
-              className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-b-4 border-x-0 border-t-0"
-            >
-              <div className="relative w-full aspect-[16/9] col-span-1">
-                <Image
-                  src="/assets/images/image15.png"
-                  alt="left image"
-                  fill
-                  className="object-cover"
-                />
+          <div>
+            <div>
+              <div className="p-0 m-0 border-b-4 b-generic-viridian">
+                <button className="bg-generic-viridian text-white px-6 py-2">Upcomings</button>
               </div>
-              <div className="flex justify-content-center items-center w-full col-span-2">
-                <p className="mt-2 ml-4 text-left text-lg font-semibold">{leftImageTitle}</p>
+              <div className="flex flex-row justify-around items-center my-4">
+                {['Games', 'Phone', 'Movies', 'More'].map((e) => (
+                  <button type="button" key={e} className="generic-viridian font-bold">
+                    <span className={`${e === 'Games' ? 'border-b-4 b-generic-viridian' : ''}`}>
+                      {e}
+                    </span>
+                  </button>
+                ))}
               </div>
             </div>
-          ))}
+
+            <div className="border-2 b-generic-viridian p-2">
+              {[0, 1, 2, 3, 4, 5].map((e) => (
+                <div
+                  key={e}
+                  className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-b-4 border-x-0 border-t-0"
+                >
+                  <div className="relative w-full aspect-[16/9] col-span-1 h-full">
+                    <Image
+                      src="/assets/images/image15.png"
+                      alt="left image"
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
+                  <div className="flex justify-content-center items-center w-full col-span-2">
+                    <p className="mt-2 ml-4 text-left text-lg font-semibold">{leftImageTitle}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
 
           <div className="border-gray-400 border w-full h-[230px] my-4"></div>
         </section>
