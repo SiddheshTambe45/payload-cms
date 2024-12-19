ba699f19015e15aed0e1fe6cbeeede149cb182a7
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-12-19 17:22:07 +0530

    improved styling and css, made some components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -70,12 +70,15 @@
 
 // export default Page
 
+import Ads from '@/components/Ads'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Page = () => {
   const leftImageTitle = 'Nintendo Museum – A Journey Through Time Nintendo Muse'
+  const test =
+    'Nintendo Museum – A Journey Through Time Nintendo Muse A Journey Through Time Nintendo Muse'
   const rightImageTitle = 'Exploring the Legacy of Nintendo'
 
   {
@@ -107,21 +110,21 @@ const Page = () => {
 
           {/* Right Div */}
           <div className="col-span-1 flex flex-col items-start">
-            <div className="relative w-full h-[400px]">
+            <Link className="relative w-full h-[400px]" href="/games/123">
               <Image
                 src="/assets/images/image16.png"
                 alt="right image"
                 fill
                 className="object-cover object-right"
               />
-            </div>
+            </Link>
             <p className="mt-2 text-left text-lg font-semibold">{rightImageTitle}</p>
           </div>
         </div>
 
         <div className="featured-blogs-lower grid grid-cols-1 lg:grid-cols-4 gap-4 mt-10">
           {[0, 1, 2, 3].map((e) => (
-            <div key={e} className="flex flex-col items-start">
+            <Link key={e} className="flex flex-col items-start p-0 m-0" href="/media/123">
               <div className="relative w-full aspect-[16/9]">
                 <Image
                   src="/assets/images/image15.png"
@@ -131,7 +134,7 @@ const Page = () => {
                 />
               </div>
               <p className="mt-2 text-left text-lg font-semibold">{leftImageTitle}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </section>
@@ -140,13 +143,13 @@ const Page = () => {
         // ads
       }
 
-      <section className="Tech-Deals mt-10">
-        <h5 className="h5 generic-viridian">
+      <section className="Tech-Deals mt-10 p-3">
+        <Link className="h5 generic-viridian" href="/tech-deals">
           <span className="font-bold">Tech Deals</span> | See All
-        </h5>
+        </Link>
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mt-5">
           {[0, 1, 2, 3].map((e) => (
-            <div key={e} className="flex flex-col items-start">
+            <Link key={e} className="flex flex-col items-start" href="/tech-deals/123">
               <div className="relative w-full aspect-[16/9]">
                 <Image
                   src="/assets/images/image15.png"
@@ -156,18 +159,18 @@ const Page = () => {
                 />
               </div>
               <p className="mt-2 text-left text-lg font-semibold">{leftImageTitle}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </section>
 
-      <section className="Mobile-Deals mt-10">
-        <h5 className="h5 generic-viridian">
+      <section className="Mobile-Deals mt-10 p-3">
+        <Link className="h5 generic-viridian" href="/mobile-deals">
           <span className="font-bold">Mobile Deals</span> | See All
-        </h5>
+        </Link>
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 mt-5">
           {[0, 1, 2, 3].map((e) => (
-            <div key={e} className="flex flex-col items-start">
+            <Link key={e} className="flex flex-col items-start" href="/mobile-deals/123">
               <div className="relative w-full aspect-[16/9]">
                 <Image
                   src="/assets/images/image15.png"
@@ -177,7 +180,7 @@ const Page = () => {
                 />
               </div>
               <p className="mt-2 text-left text-lg font-semibold">{leftImageTitle}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </section>
@@ -188,19 +191,20 @@ const Page = () => {
 
       <section className="popular-recent mt-10 lg:grid lg:grid-cols-3">
         <section className="lg:col-span-2 px-3">
-          <div className="border border-y-2 border-x-0 flex flex-row gap-8 my-4">
-            <button type="button" className="h6 generic-viridian">
+          <div className="border border-y-2 border-x-0 border-black flex flex-row gap-8 my-4 py-2">
+            <button type="button" className="h6 generic-viridian font-bold">
               Popular
             </button>
-            <button type="button" className="h6 generic-viridian">
+            <button type="button" className="h6 generic-viridian font-bold">
               Recent
             </button>
           </div>
 
           {[0, 1, 2, 3, 4, 5].map((e) => (
-            <div
+            <Link
               key={e}
-              className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-b-4 border-x-0 border-t-0"
+              className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-b-2 border-x-0 border-t-0 border-black"
+              href="/media/111"
             >
               <div className="relative w-full aspect-[16/9] col-span-1">
                 <Image
@@ -213,7 +217,7 @@ const Page = () => {
               <div className="flex justify-content-center items-center w-full col-span-2">
                 <p className="mt-2 ml-4 text-left text-lg font-semibold">{leftImageTitle}</p>
               </div>
-            </div>
+            </Link>
           ))}
         </section>
         <section className="lg:col-span-1 px-3">
@@ -236,22 +240,9 @@ const Page = () => {
             </div>
 
             <div className="border-2 b-generic-viridian p-2">
-              {[0, 1, 2, 3, 4, 5].map((e) => (
-                <div
-                  key={e}
-                  className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-b-4 border-x-0 border-t-0"
-                >
-                  <div className="relative w-full aspect-[16/9] col-span-1 h-full">
-                    <Image
-                      src="/assets/images/image15.png"
-                      alt="left image"
-                      fill
-                      className="object-cover"
-                    />
-                  </div>
-                  <div className="flex justify-content-center items-center w-full col-span-2">
-                    <p className="mt-2 ml-4 text-left text-lg font-semibold">{leftImageTitle}</p>
-                  </div>
+              {[0].map((e) => (
+                <div className="m-0 p-0" key={e}>
+                  <Ads />
                 </div>
               ))}
             </div>
---
+++ components/Ads.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Ads = () => {
+  const leftImageTitle =
+    'leftImageTitle leftImageTitle leftImageTitle leftImageTitle leftImageTitle'
+  return (
+    <>
+      <div className="border-black border w-full h-[300px] my-4"></div>
+
+      {[0, 1, 2, 3, 4, 5].map((e) => (
+        <Link
+          key={e}
+          className="flex-row items-start grid grid-cols-3 my-3 w-full py-2 border border-black border-b-2 border-x-0 border-t-0"
+          href="/tech-deals/122"
+        >
+          <div className="relative w-full aspect-[16/9] col-span-1 h-full">
+            <Image
+              src="/assets/images/image15.png"
+              alt="left image"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="flex justify-content-center items-center w-full col-span-2">
+            <p className="mt-2 ml-4 text-left text-lg font-semibold">
+              {leftImageTitle.length < 30
+                ? leftImageTitle
+                : leftImageTitle.substring(0, 70).concat('...')}
+            </p>
+          </div>
+        </Link>
+      ))}
+    </>
+  )
+}
+
+export default Ads
